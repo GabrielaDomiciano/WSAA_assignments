07823461d25140ed44a7a15cbb029a5aa0c79d2b
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2025-03-26 20:48:52 +0000

    API

---
+++ assignment03-cso.py
@@ -0,0 +1,9 @@
+# Gabriela Domiciano
+
+import requests
+
+url = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en'
+response = requests.get(url)
+
+with open('cso.json', 'wb') as f:
+    f.write(response.content)
